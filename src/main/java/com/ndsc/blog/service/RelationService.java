7ccc65c97f516fd658f867fd51a6fa5c9b29e9c5
fearzero <fearzhao@gmial.com>
2019-09-14 16:51:56 +0800

    更新了一下/login的返回值

--- src/main/java/com/ndsc/blog/service/RelationService.java
+++
@@ -1,9 +0,0 @@
-package com.ndsc.blog.service;
-
-import com.ndsc.blog.entity.Relation;
-
-public interface RelationService {
-    public int addFollow(int blogerId,int fansId);
-    public int delFollow(int blogerId,int fansId);
-    public Relation viewFollow(int blogerId,int fansId);
-}
