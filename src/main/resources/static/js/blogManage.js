395ac093a90fe012383c49a6b986e7f0e02ba41b
DENGCW97 <769711277@qq.com>
2019-09-19 18:34:18 +0800

    修改managejs

--- src/main/resources/static/js/blogManage.js
+++ src/main/resources/static/js/blogManage.js
@@ -12,46 +12,4 @@ $(function () {
 
     })
 
-    $.ajax({
-        url: "http://localhost:8080/selectUidUsafeUinfoBlog",
-        type: "post",
-        data: {"userId": userParameter},
-        dataType: "json",
-        success: function (data1) {
-            if (data1 > 0) {
-                $.ajax({
-                    url:"/searchUserBlogs",
-                    dataType: "json",
-                    success:function (data) {
-                        for (var i = 0;i < data.length ; i++) {
-
-                        }
-                    }
-                })
-            } else {
-            }
-        }
-    });
-
-    $.ajax({
-        url: "/getUserId",
-        type: "post",
-        dataType: "json",
-        success: function (data1) {
-            if (data1 > 0) {
-                $.ajax({
-                    url:"/searchUserBlogs",
-                    dataType: "json",
-                    success:function (data) {
-                        for (var i = 0;i < data.length ; i++) {
-
-                        }
-                    }
-                })
-            } else {
-            }
-        }
-    });
-
-
 })
